dd9b37853023f53593a8a917c25879cc6cbbb1e0
Martin <martin.schultz@goodstag.com>
2020-05-03 23:37:23 +0200

    allow auth via swagger

--- src/main/java/de/kottilabs/todobackend/config/SpringFoxConfig.java
+++ src/main/java/de/kottilabs/todobackend/config/SpringFoxConfig.java
@@ -1,42 +1,32 @@
 package de.kottilabs.todobackend.config;
 
-import com.google.common.collect.Lists;
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig {
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).securityContexts(Lists.newArrayList(securityContext())).select()
-				.apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
-	}
-
-	private ApiKey apiKey() {
-		return new ApiKey("JWT", "Authorization", "header");
-	}
-
-	private springfox.documentation.spi.service.contexts.SecurityContext securityContext() {
-		return springfox.documentation.spi.service.contexts.SecurityContext.builder().securityReferences(defaultAuth())
-				.forPaths(PathSelectors.any()).build();
-	}
-
-	List<SecurityReference> defaultAuth() {
-		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-		authorizationScopes[0] = authorizationScope;
-		return Lists.newArrayList(new SecurityReference("JWT", authorizationScopes));
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build()
+				.securitySchemes(Collections.singletonList(new ApiKey("JWT", "Authorization", "header")))
+				.securityContexts(Collections.singletonList(SecurityContext.builder()
+						.securityReferences(Collections.singletonList(
+								SecurityReference.builder().reference("JWT").scopes(new AuthorizationScope[0]).build()))
+						.build()));
 	}
 
 }
